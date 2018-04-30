cdaeeb3d67975f467edc2c8e6b67ae1c31ee8fa5
Peter Seger <segerpeter07@gmail.com>
2018-04-29 23:58:03 -0400

    Try 2

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os.environ
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +7,6 @@ def hello_world():
     return render_template('test.html')
 
 if __name__ == '__main__':
-    app.run(host='35.227.23.25', port="5000")
+    HOST = '0.0.0.0' if 'PORT' in os.environ else '127.0.0.1'
+PORT = int(os.environ.get('PORT', 5000)))
+app.run(host=HOST, port=PORT, threaded=True)
