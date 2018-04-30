b49aa152c4aa979b416938c6e9391999123a4ae7
Peter Seger <segerpeter07@gmail.com>
2018-04-30 00:02:11 -0400

    Changing port

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def hello_world():
 
 if __name__ == '__main__':
     HOST = '0.0.0.0' if 'PORT' in os.environ else '127.0.0.1'
-PORT = int(os.environ.get('PORT', 5000)))
-app.run(host=HOST, port=PORT, threaded=True)
+    PORT = int(os.environ.get('PORT', 5000)))
+    app.run(host=HOST, port=PORT, threaded=True)
